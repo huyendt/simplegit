4df692ddd197886dc36d6248323bed0bb7bcd411
Huyen Duong <huyendt1911@gmail.com>
2014-08-11 15:18:00 +0700

    Edit HelloGitBranch

--- simple-git/src/main/java/com/telsoft/outsourcing/sample/simple_git/HelloGitBranch.java
+++ simple-git/src/main/java/com/telsoft/outsourcing/sample/simple_git/HelloGitBranch.java
@@ -2,6 +2,6 @@ package com.telsoft.outsourcing.sample.simple_git;
 
 public class HelloGitBranch {
 	public static void main(String args[]) throws Exception {
-		System.out.println("Hello Git Branch!");
+		System.out.println("Hello Git Branch lalala!");
 	}
 }
