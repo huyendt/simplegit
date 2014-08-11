96f55d7da3c6cc24e67c74f0fac9720f5ca10527
Huyen Duong <huyendt1911@gmail.com>
2014-08-11 15:02:19 +0700

    Create file HelloGitBranch.java

---
+++ simple-git/src/main/java/com/telsoft/outsourcing/sample/simple_git/HelloGitBranch.java
@@ -0,0 +1,7 @@
+package com.telsoft.outsourcing.sample.simple_git;
+
+public class HelloGitBranch {
+	public static void main(String args[]) throws Exception {
+		System.out.println("Hello Git Branch!");
+	}
+}
