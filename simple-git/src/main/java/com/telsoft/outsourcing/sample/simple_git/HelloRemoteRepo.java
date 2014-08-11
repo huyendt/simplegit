ac1a8c783be853e0f4b4c1c2a9939e1160db21df
Huyen Duong <huyendt1911@gmail.com>
2014-08-11 15:39:59 +0700

    Add HelloRemoteRepo

---
+++ simple-git/src/main/java/com/telsoft/outsourcing/sample/simple_git/HelloRemoteRepo.java
@@ -0,0 +1,7 @@
+package com.telsoft.outsourcing.sample.simple_git;
+
+public class HelloRemoteRepo {
+	public static void main(String args[]) throws Exception {
+		System.out.println("Hello Remote Repo!");
+	}
+}
