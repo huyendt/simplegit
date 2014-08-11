3e159d282b25f06f0d4eebf1b6960a4a9888033e
huyendt <huyendt1911@gmail.com>
2014-08-11 15:42:18 +0700

    Update HelloRemoteRepo.java

--- simple-git/src/main/java/com/telsoft/outsourcing/sample/simple_git/HelloRemoteRepo.java
+++ simple-git/src/main/java/com/telsoft/outsourcing/sample/simple_git/HelloRemoteRepo.java
@@ -2,6 +2,6 @@ package com.telsoft.outsourcing.sample.simple_git;
 
 public class HelloRemoteRepo {
 	public static void main(String args[]) throws Exception {
-		System.out.println("Hello Remote Repo!!!!");
+		System.out.println("Hello Remote Repo edit!!!!");
 	}
 }
